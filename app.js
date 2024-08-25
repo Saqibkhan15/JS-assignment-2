ce324ae2de107d3d56f5cf7365d5a4c99e396afa
Saqibkhan15 <aqib12u@gmail.com>
2024-08-25 22:49:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// Assignment chapter no 3 to  chapter no 9
+// Chapter no 3
+
+// Q1
+// var age = " im 29 years old"
+// alert(age)
+
+// Q2
+// var note = " you have visited this site 15 times"
+// alert(note)
+// Q3
+// var birthYear = 1990;
+// document.write("My birth year is " + birthYear + " \nDeclare variable and assign birth year ");
+
+//Q4
+
+// var visitorName = "John Doe";
+// var productTitle = "T-shirt";
+// var quantity = 5;
+
+// document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
