48e30fa0c1f64c397b67bb8f5f566ac116bdb815
Saqibkhan15 <aqib12u@gmail.com>
2024-08-29 21:08:49 +0500

    Update app.js

--- app.js
+++ app.js
@@ -19,3 +19,23 @@
 // var quantity = 5;
 
 // document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
+
+chapter 4
+// //Declare 3 variables in one statement
+// var a, b, c;
+
+//Legal variable names
+// var myVariable, _anotherVariable, $thirdVariable, name123, camelCaseVariable;
+
+// //Illegal variable names
+// var 123variable, my-variable, my variable, my@variable, class;
+
+
+// document.write("<h2>Rules for naming JS variables</h2>");
+// document.write("<h6>Variable names can only contain letters, numbers, underscores (_), and dollar signs ($)</h6>");
+// document.write("<h6>For example: $my_1stVariable<h6>");   
+
+// document.write("<h6>Variables must begin with a letter, underscore (_), or dollar sign ($)<h6>");
+// document.write("<h6>For example: $name, _name, or name<h6>");
+// document.write("<h6>Variable names are case sensitive<h6>");
+// document.write("<h6>Variable names should   not be JavaScript keywords<h6>");
